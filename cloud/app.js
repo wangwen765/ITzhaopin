d7134fcefecfbee2d831f1cc34c7c96b210371d9
wangwen765 <zhouaaal2@swlsg.com>
2015-07-16 01:31:01 +0800

    Update app.js

--- cloud/app.js
+++ cloud/app.js
@@ -96,7 +96,7 @@ app.post('/',function(req, res){
 	var Ppone=req.query.Ppone;
 	var Pmail=req.query.Pmail;
 	var jplevel=req.query.jplevel;
-	res.render('hello', { message: pposition & Pname });
+	res.render('hello', { message: pposition & "-" & Pname });
 	if(pposition && pposition.trim() !=''){
 		//Save visitor
 		var visitor = new Visitor();
